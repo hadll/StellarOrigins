de2b24a0451408c12e0f89bb5b65a314baeb9459
Haddle <94085928+hadll@users.noreply.github.com>
2025-11-20 01:25:06 +1100

    init commit

---
+++ src/client/java/haddle/stellarorigins/StellarOriginsClient.java
@@ -0,0 +1,10 @@
+package haddle.stellarorigins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StellarOriginsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
