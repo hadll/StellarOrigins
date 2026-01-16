ce19741c375b0c23cedf4a90939c64ca401e41a0
Haddle <94085928+hadll@users.noreply.github.com>
2026-01-17 03:06:40 +1100

    made the taser hold to use

--- src/main/java/haddle/stellarorigins/item/custom/vials/StarpowerConsumable.java
+++ src/main/java/haddle/stellarorigins/item/custom/vials/StarpowerConsumable.java
@@ -15,6 +15,7 @@ import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
@@ -25,29 +26,60 @@ import java.util.List;
 
 public class StarpowerConsumable extends Item {
 
+    private final int useTime = 20;
+
     public StarpowerConsumable(Settings settings) {
         super(settings.maxDamage(3));
     }
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if(!world.isClient() && hand == Hand.MAIN_HAND) {
-            ItemStack stack = user.getEquippedStack(EquipmentSlot.MAINHAND);
+        if(hand == Hand.MAIN_HAND) {
+            user.setCurrentHand(hand);
+            return TypedActionResult.consume(user.getStackInHand(hand));
+        }
+        return super.use(world, user, hand);
+    }
+
+    @Override
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        int usedTicks = getMaxUseTime(stack) - remainingUseTicks;
+        System.out.println(remainingUseTicks);
+        if (!world.isClient && user instanceof PlayerEntity player && usedTicks >= useTime) {
             stack.damage(1, user, (e) -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
             StellarOrigins.STARPOWER_HELPER.consumeStarpower(user, 30);
             StellarOrigins.STARPOWER_HELPER.updateStarpower(user);
             world.playSound(null, user.getX(), user.getY(), user.getZ(), SOSounds.RECHARGE_SOUND, SoundCategory.PLAYERS, 1f, (float)StellarOrigins.STARPOWER_HELPER.getStarpower(user)/70F);
             user.damage(SODamageSources.vial(),  4);
-            user.getItemCooldownManager().set(this, 15);
-
+            player.getItemCooldownManager().set(this, 15);
         }
-        return super.use(world, user, hand);
+    }
+
+    @Override
+    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        if (world.isClient) return;
+
+        int usedTicks = getMaxUseTime(stack) - remainingUseTicks;
+
+        if (usedTicks >= useTime) {
+            user.stopUsingItem();
+        }
     }
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
         tooltip.add(Text.literal("Restores 30 Starpower").setStyle(Style.EMPTY.withColor(MathHelper.packRgb(230, 118, 252))));
         super.appendTooltip(stack, world, tooltip, context);
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack) {
+        return 72000;
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BOW;
     }
 
     @Override
